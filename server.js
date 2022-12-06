c4dc5e1c1fff29b591190accc1caae9b3fbb7975
Mohammad Akbar MOHAMMADI <MOHAMMADI@MOHAMMADI.ORG>
2022-12-06 16:30:48 -0500

    The Homework is Done for Second Time to show the required message

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//Greetings
+//=========
+app.get("/", (req, res) => {
+  res.send(`<h1>Hello, stranger</h1>`);
+});
+app.get("/hello/:name", (req, res) => {
+  res.send(`<h1>Wow! Hello There, </h1><h2>${req.params.name}</h2>`);
+});
+
+//Tip Calculator
+//==============
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const payAndTip = req.params.total * (req.params.tipPercentage / 100);
+  res.send(
+    `<h1>Thank You for your Payment </h1><h2>And your Tip of ${payAndTip} is appreciated!</h2>`
+  );
+});
+
+//Magic 8 Ball
+//============
+app.get("/magic/:input", (req, res) => {
+  const responses = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+  ];
+  const input = req.params.input;
+  const response = responses[Math.floor(Math.random() * responses.length)];
+  res.send(`<h3>${input} </h3> <h2>${response}</h2>`);
+});
+
+app.listen(port, () => {
+  console.log(`App running at port ${port}`);
+});
